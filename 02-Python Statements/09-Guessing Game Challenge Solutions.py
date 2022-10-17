ce439eb7f9b5a7697e5d9f46d8098d2b92e6cfea
Danica Popadic <dpopadic@lendflow.com>
2022-10-17 14:27:30 +0200

    updated solution

--- 02-Python Statements/09-Guessing Game Challenge Solutions.py
+++ 02-Python Statements/09-Guessing Game Challenge Solutions.py
@@ -1,37 +1,42 @@
 import random
 
 number = random.randint(1, 100)
-guesses = [0]
+new_user_number = 0
+old_user_number = None
+guesses = 0
 
-print('This is a number guessing game!')
+while new_user_number != number:
+    new_user_number = input('Choose a number between 0 and 100.\n')
 
-while guesses[-1] != number:
-    guesses.append(input('Choose a number between 1 and 100.'))
-
-    if guesses[-1].isnumeric():
-        guesses[-1] = int(guesses[-1])
+    # Check is it a number
+    if new_user_number.isdecimal():
+        new_user_number = int(new_user_number)
     else:
-        print(f'{guesses[-1]} is not a number!')
-        guesses.pop(-1)
+        print(f'{new_user_number} is not a number!')
         continue
 
-    if 1 > guesses[-1] or guesses[-1] > 100:
-        print('OUT OF BOUNDS')
-        guesses.pop(-1)
-        continue
-
-    if guesses[-1] == number:
+    # Check is it a right guess
+    if new_user_number == number:
         break
 
-    if len(guesses) < 3:
-        if abs(number - guesses[-1]) > 10:
+    # Check is it out of bounds
+    if 1 > new_user_number or new_user_number > 100:
+        print('OUT OF BOUNDS!')
+        continue
+
+    guesses += 1
+
+    if old_user_number is None:
+        if abs(number - new_user_number) > 10:
             print('COLD!')
         else:
             print('WARM!')
     else:
-        if abs(number - guesses[-2]) > abs(number - guesses[-1]):
+        if abs(number - old_user_number) > abs(number - new_user_number):
             print('WARMER!')
         else:
             print('COLDER!')
 
-print(f'Success after {len(guesses) - 1} guesses')
+    old_user_number = new_user_number
+
+print(f'Success after {guesses} guesses')
