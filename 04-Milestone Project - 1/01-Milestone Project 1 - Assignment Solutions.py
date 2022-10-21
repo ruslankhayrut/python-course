f1b27d03c5462ccf4a03e958bbce8b0f84117571
Danica Popadic <dpopadic@lendflow.com>
2022-10-21 09:59:56 +0200

    fix of milestone project 1

--- 04-Milestone Project - 1/01-Milestone Project 1 - Assignment Solutions.py
+++ 04-Milestone Project - 1/01-Milestone Project 1 - Assignment Solutions.py
@@ -1,19 +1,20 @@
 from typing import Union
 
 
+board = ['-' for _ in range(9)]
+winner = False
+current_player = 'X'
+
+
 def print_board():
-    print(f'{BOARD[1]} {BOARD[2]} {BOARD[3]}')
-    print(f'{BOARD[4]} {BOARD[5]} {BOARD[6]}')
-    print(f'{BOARD[7]} {BOARD[8]} {BOARD[9]}')
+    for i in range(0, 9, 3):
+        print(' '.join(board[i:i+3]))
 
 
-def user_input(player: str) -> int:
-    position = 'wrong'
+def user_input() -> int:
+    print(f'\nPlayer {current_player} turn')
 
-    print(f'\nPlayer {player} turn')
-
-    while position.isdigit() == False:
-
+    while True:
         value = input('Choose a position: (1-9)\n')
         print()
 
@@ -21,17 +22,18 @@ def user_input(player: str) -> int:
             print('Not a digit, try again.')
             continue
 
-        if int(value) not in range(1, 10):
+        position = int(value) - 1
+
+        if position not in range(0, 9):
             print('Not in range, try again.')
             continue
 
-        if BOARD[int(value)] != '-':
+        print(board[position])
+        if board[position] != '-':
             print('Position is occupied, try again.')
             continue
 
-        position = value
-
-        return int(position)
+        return position
 
 
 def set_has_win(the_set: set) -> bool:
@@ -41,51 +43,48 @@ def set_has_win(the_set: set) -> bool:
 def check_win() -> Union[bool, str]:
     winner = False
     # horizontal check
-    for i in range(1, 8, 3):
-        if set_has_win(set((BOARD[i], BOARD[i+1], BOARD[i+2]))):
-            return BOARD[i]
+    for i in range(0, 7, 3):
+        if set_has_win({board[i], board[i+1], board[i+2]}):
+            return board[i]
 
     # vertical check
-    for i in range(1, 4):
-        if set_has_win(set((BOARD[i], BOARD[i+3], BOARD[i+6]))):
-            return BOARD[i]
+    for i in range(0, 3):
+        if set_has_win({board[i], board[i+3], board[i+6]}):
+            return board[i]
 
     # diagonal check
-    if set_has_win(set((BOARD[1], BOARD[5], BOARD[9]))):
-        return BOARD[1]
+    if set_has_win({board[0], board[4], board[8]}):
+        return board[0]
 
-    if set_has_win(set((BOARD[3], BOARD[5], BOARD[7]))):
-        return BOARD[3]
+    if set_has_win({board[2], board[4], board[6]}):
+        return board[2]
 
     return winner
 
 
 def game():
-    winner = False
-    current_turn = 'X'
+    global winner, current_player
     counter = 0
 
     print('Starting board:')
     print_board()
 
     while not winner and counter != 9:
-        position = user_input(current_turn)
-
-        BOARD[position] = current_turn
+        position = user_input()
+        board[position] = current_player
 
         print_board()
 
         if counter > 3:
             winner = check_win()
 
-        current_turn = 'X' if current_turn == 'O' else 'O'
+        current_player = 'X' if current_player == 'O' else 'O'
         counter += 1
 
     if winner:
-        print(f'The winner is {winner} player! Game over, thanks for playing.')
+        print(f'\nThe winner is {winner} player! Thanks for playing.')
     else:
-        print('Game over! No more playing fields!')
+        print('\nGame over! No more playing fields!')
 
 
-BOARD = ['placeholder'] + ['-' for _ in range(9)]
 game()
