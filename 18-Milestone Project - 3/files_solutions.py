8f1d6995fb4c0ea3f2771de6e8d6fcfc1f584fa5
Danica Popadic <dpopadic@lendflow.com>
2022-11-08 11:39:13 +0100

    Milestone 3/Files solution

--- 18-Milestone Project - 3/files_solutions.py
+++ 18-Milestone Project - 3/files_solutions.py
@@ -17,18 +17,63 @@ class DataSorter:
         for column in self.data.columns:
             print(column)   
 
-    def sort(self, columns, asc:bool = True):
+    def sort(self):
+        print('Name of the columns:')
+        self.print_columns()
+
+        columns = []
+        while True:
+            column = input('Input the name of columns to sort by or press "q" to quit and continue: ')
+
+            if column == 'q':
+                break
+
+            if column not in self.data.columns:
+                print('Unknown column, try again')
+                continue
+
+            if column in columns:
+                print('Already selected that column, try again')
+                continue
+
+            columns.append(column)
+
+        while True:
+            asc = input('Order ascending? y/n\n')
+
+            if asc in ['y','n']:
+                break
+                
+            print('Unknown choice, try again')
+
+        self.sort_data(columns, asc == 'y')
+        self.save()
+
+        print('Sorted data has been saved to current file.')
+
+
+    def sort_data(self, columns, asc:bool = True):
         self.data.sort_values(columns, axis=0, ascending=asc,inplace=True,na_position='first')
 
     def save(self):
         self.data.to_csv(self.path)
+
+    def start(self):
+        while True:
+            action = input('Press "s" for sorting or "q" to quit:\n')
+            if action not in ['s', 'q']:
+                print('Unknown action, try again.')
+                continue
+
+            if action == 'q':
+                break
+
+            self.sort()
+
         
 
-# file_name = input('Input path to the csv file:')
 file_name = 'dataset.csv'
 data_sorter = DataSorter(file_name)
-data_sorter.print_columns()
-data_sorter.sort(['compound_name', 'species'])
-data_sorter.save()
+data_sorter.start()
 
     
